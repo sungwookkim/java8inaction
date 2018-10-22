54696432b497aa708cebf2d6753a3304f65e773a
sinnakeWEB <sinnakeWEB@sinnake.test.co.kr>
2018-10-23 05:35:51 +0900

    챕터 3 추가

--- Java8 In Action/src/Part1/Chapter3/Chapter3_3_2/Main_3_2.java
+++ Java8 In Action/src/Part1/Chapter3/Chapter3_3_2/Main_3_2.java
@@ -55,11 +55,10 @@ package Part1.Chapter3.Chapter3_3_2;
  */
 public class Main_3_2 {
 	
-	public static class subMain_3_2 {
-		public static void process(Runnable r) {
-			r.run();
-		}
+	public static void process(Runnable r) {
+		r.run();
 	}
+
 	
 	/*
 	 * 3.2.1 함수형 인터페이스 예제
@@ -70,23 +69,52 @@ public class Main_3_2 {
 	static void runnable() {
 		// 람다 사용.
 		Runnable r1 = () -> System.out.println("hello hell 1?");		
-		subMain_3_2.process(r1);
+		process(r1);
 		
 		// 익명 클래스 사용.
-		Runnable r2 = new Runnable() {
-			
+		process(new Runnable() {
+
 			@Override
 			public void run() {
 				System.out.println("hello hell 2?");
+				
 			}
-		};
-		subMain_3_2.process(r2);
+		});
 		
 		// 직접 전달된 람다 표현식.
-		subMain_3_2.process(() -> System.out.println("hello hell 3?"));
+		process(() -> System.out.println("hello hell 3?"));
 	}
+	
+	/*
+	 * 3.2.2 함수 디스크립터
+	 * 
+	 * 함수형 인터페이스의 추상 메서드 시그너처(signature)는 람다 표현식의 시그너처를 가리킨다.
+	 * 람다 표현식의 시그너처를 서술하는 메서드를 함수 디스크립터(function descriptor)라고 한다.
+	 * 예를 들어
+	 * 		Runnable에 유일한 추상 메서드인 run메서드는 인수와 반환값이 없으므로
+	 * 		Runnable 인터페이스는 인수와 반환값이 없는 시그너처로 생각할 수 있다.
+	 * 
+	 * 람다 표현식의 유효성을 확인하는 방법은 일단 람다 표현식은 변수에 할당하거나 함수형 인터페이스를
+	 * 인수로 받는 메서드로 전달할 수 있으며, 함수형 인터페이스의 추상 메서드와 같은 시그너처를 갖는다는
+	 * 사실을 기억하자.
+	 * 
+	 * 함수형 인터페이스에 추상 메서드로 전달 되는 람다 표현식은 해당 메서드의 반환값 혹은 타입, 인수 타입 혹은 갯수에
+	 * 맞춰서 표현식을 전달 해야 한다.
+	 * 		Runnable의 run 메서드는 반환값은 void이고 인수는 없다. 
+	 * 		위 예제 코드에서
+	 * 			process(() -> System.out.println("hello hell 3?"));
+	 * 		코드를 람다 표현식으로 직접 전달 하였다.  		
+	 * 		여기서 직접 전달된 람다 표현식을 분석 하자면 () 인수가 없고 메서드 바디 영역에서는 println메서드만 호출하고
+	 * 		반환값이 없다. 즉, run 함수 시그너처와 같은 인수가 없고 반환값이 없는 형태와 동일하므로 올바른 람다 표현식이다.
+	 */
 	public static void main(String[] args) {
 		runnable();
 	}
 
+	/*
+	 * @FunctionalInterface는 무엇인가?
+	 * 새로운 자바 API를 살펴보면 함수형 인터페이스에 @FunctionalInterface라는 어노테이션이 추가 되었다.
+	 * @FunctionalInterface는 함수형 인터페이스임을 가리키는 어노테이션이다.
+	 * @FunctionalInterface로 인터페이스를 선언했지만 실제로 함수형 인터페이스가 아니면 컴파일러가 에러를 발생시킨다.
+	 */
 }
