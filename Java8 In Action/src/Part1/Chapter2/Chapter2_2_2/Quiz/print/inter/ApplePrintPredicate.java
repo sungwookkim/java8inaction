11df3131af22615a71a74fda866837f7050aa31a
sinnakeWEB <sinnakeWEB@sinnake.test.co.kr>
2018-10-29 11:48:21 +0900

    새로 푸쉬

--- Java8 In Action/src/Part1/Chapter2/Chapter2_2_2/Quiz/print/inter/ApplePrintPredicate.java
+++
@@ -1,7 +0,0 @@
-package Part1.Chapter2.Chapter2_2_2.Quiz.print.inter;
-
-import Part1.Chapter2.Chapter2_2_2.entity.AppleEntity;
-
-public interface ApplePrintPredicate {
-	String print(AppleEntity apple);
-}
--- Java8 In Action/src/Part1/Chapter2/Chapter2_2_3/filters/inter/Predicate.java
+++
@@ -1,5 +0,0 @@
-package Part1.Chapter2.Chapter2_2_3.filters.inter;
-
-public interface Predicate<T> {
-	boolean test(T t);
-}
